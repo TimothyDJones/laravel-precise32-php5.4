7aedc77285d2c8a3af52fb1c6c9c6c7e1ec49708
Tim Jones <tdjones74021@yahoo.com>
2015-03-27 15:11:24 -0500

    Add Puppet module to globally install Composer.

---
+++ puppet/modules/composer/manifests/init.pp
@@ -0,0 +1,13 @@
+class composer {
+	exec { "composer-install":
+		unless => "[ -f /usr/local/bin/composer ]",
+		require => Package["php5-cli"],
+		command => "/usr/bin/curl -s https://getcomposer.org/installer | /usr/bin/php -- --install-dir=/usr/local/bin",
+	}
+	
+	exec { "composer-global":
+		unless => "[ -f /usr/local/bin/composer ]",
+		require => Exec["composer-install"],
+		command => "sudo mv /usr/local/bin/composer.phar /usr/local/bin/composer",
+	}
+}
