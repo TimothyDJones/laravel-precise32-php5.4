0084f484c02e80cbec45ba4b0834603be68b59b9
Tim Jones <tdjones74021@yahoo.com>
2015-03-27 11:53:45 -0500

    Remove 'xdebug.ini' file copy, because this is now handled by separate 'xdebug' module.

--- puppet/modules/apache/manifests/init.pp
+++ puppet/modules/apache/manifests/init.pp
@@ -30,16 +30,4 @@ class apache {
 		source => "${config::filepath}/apache/default",
 		target => "/etc/apache2/sites-available/default",
 	}
-	
-	# Copy the XDebug configuration file for remote debugging.
-	file { "/etc/php5/apache2/conf.d/20-xdebug.ini":
-		notify => Service["apache2"],
-		mode => 644,
-		owner => "root",
-		group => "root",
-		require => Package["apache2"],
-		source => "${config::filepath}/custom/20-xdebug.ini",
-		target => "/etc/php5/apache2/conf.d/20-xdebug.ini",
-	}
-	
 }
